9c766873b70c789e025aa64fde626e2f7eaa1c68
JohnQuezada <johnaquezada@gmail.com>
2022-03-14 20:12:43 -0400

    NASA API Update

--- src/main/java/com/careerdevs/HelloInternet/controllers/NasaController.java
+++ src/main/java/com/careerdevs/HelloInternet/controllers/NasaController.java
@@ -1,20 +1,52 @@
 package com.careerdevs.HelloInternet.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/nasa")
+@RequestMapping("/api/nasa")
 public class NasaController {
 
-    private String myNasaKey = "uJvcfcd7qUcb0d9LbzBjZMNyfxXspR3girJPjKNH";
-    private String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
+    @Autowired
+    private Environment env;
+
+//    private String myNasaKey = "uJvcfcd7qUcb0d9LbzBjZMNyfxXspR3girJPjKNH";
+//
+//    private String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
 
     @GetMapping("/apod")
-    public Object apodHandler (RestTemplate restTemplate) {
-        return restTemplate.getForObject(nasaApodEndpoint, Object.class);
+    private Object apodHandler (RestTemplate restTemplate) {
+
+        String apodKey = env.getProperty("NASA_API_KEY", "DEMO_KEY");
+
+        String URL = "https://api.nasa.gov/planetary/apod?api_key=" + apodKey;
+
+        Object response = restTemplate.getForObject(URL, Object.class);
+
+        System.out.println(response);
+        return response;
+        //        return restTemplate.getForObject(nasaApodEndpoint + apiKey, Object.class);
+    }
+
+//    @Override
+//    public String toString() {
+//        return "NasaController{" + "env=" + env + ", nasaApodEndpoint='" + nasaApodEndpoint + '\'' + '}';
+//    }
+
+    @GetMapping("port")
+    public String portTest () {
+
+        return env.getProperty("server.port");
+    }
+
+    @PostMapping ("/info")
+    public String apodInfo () {
+        return "NASA APOD provides daily astro-photography photos/videos";
     }
 }
---
+++ src/main/resources/README.md
@@ -0,0 +1,7 @@
+Application.properties is being used to store api keys.
+For the program to work add the following keys with valid api keys.
+
+APOD_KEY
+
+Optionally if you want to start the server on a port other than 8080 add...
+ser.port=[PORT_NUM]
